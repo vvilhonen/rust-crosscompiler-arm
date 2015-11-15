d0463ce48130c3e6265e3bb7074da5dd272a6f15
Damien Lecan <dev@dlecan.com>
2015-11-15 22:13:26 +0100

    Update for Rust 1.4.0

--- build-all.sh
+++ build-all.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+RUST_VERSION=1.4.0
+
 docker build -t dlecan/rust-x86_64-armv6 rust-x86_64-armv6
+
+docker tag dlecan/rust-x86_64-armv6:latest dlecan/rust-x86_64-armv6:$RUST_VERSION
+
+docker push dlecan/rust-x86_64-armv6
