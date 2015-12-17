fab90d5790673752b96b1973e261ce6a6a4e734a
Damien Lecan <dev@dlecan.com>
2015-12-17 23:27:33 +0100

    Build also 'stable' tag

--- build-all.sh
+++ build-all.sh
@@ -5,5 +5,6 @@ RUST_VERSION=1.5.0
 docker build -t dlecan/rust-x86_64-armv6 rust-x86_64-armv6
 
 docker tag dlecan/rust-x86_64-armv6:latest dlecan/rust-x86_64-armv6:$RUST_VERSION
+docker tag dlecan/rust-x86_64-armv6:stable dlecan/rust-x86_64-armv6:latest
 
 docker push dlecan/rust-x86_64-armv6
