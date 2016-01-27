a3e8a6d18be162a84bf9fe396cbef95841a935c6
Damien Lecan <dev@dlecan.com>
2016-01-27 08:04:22 +0100

    Rust 1.6.0

--- build-all.sh
+++ build-all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RUST_VERSION=1.5.0
+RUST_VERSION=1.6.0
 
 docker build -t dlecan/rust-x86_64-armv6 rust-x86_64-armv6
 
-docker tag dlecan/rust-x86_64-armv6:latest dlecan/rust-x86_64-armv6:$RUST_VERSION
-docker tag dlecan/rust-x86_64-armv6:stable dlecan/rust-x86_64-armv6:latest
+docker tag -f dlecan/rust-x86_64-armv6:latest dlecan/rust-x86_64-armv6:$RUST_VERSION
+docker tag -f dlecan/rust-x86_64-armv6:latest dlecan/rust-x86_64-armv6:stable
 
 docker push dlecan/rust-x86_64-armv6
